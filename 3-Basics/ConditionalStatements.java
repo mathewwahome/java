d72c34cea37fa716d2084e8ffcbbb42d6d9343eb
mathewwahome <methewwahome@gmail.com>
2024-03-11 10:47:13 +0300

    conditions and switch

---
+++ 3-Basics/ConditionalStatements.java
@@ -0,0 +1,14 @@
+public class ConditionalStatements {
+    public static void main(String[] args) {
+        int age = 20;
+        if (age >= 18) {
+            System.out.println("you can vote");
+        } else if (age < 18) {
+            System.out.println("you can`t vote");
+        } else {
+            System.out.println("unknown");
+
+        }
+        // &&-and ||-or
+    }
+}
