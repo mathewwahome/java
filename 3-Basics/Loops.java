4b49a651bbfb9948479550161edb3e5c57af08c6
mathewwahome <methewwahome@gmail.com>
2024-03-11 11:05:26 +0300

    loops

---
+++ 3-Basics/Loops.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // initialisation condition increment++
+
+        int x = scanner.nextInt();
+        for (int y = 1; y <= x; y++) {
+            System.out.println("you typed");
+
+        }
+
+        // for (int count = 1; count <= 1000; count++) {
+        // System.out.println(count + " loops");
+        // }
+    }
+}
